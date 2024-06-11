76eb523dab24a343c6fcab64fb775216652418df
PshenichkinVladimir <PshenichkinVladimir@gmail.com>
2024-06-11 15:14:25 +0300

    1

---
+++ src/main/java/net/vladimir/tutorialmod/TutorialModClient.java
@@ -0,0 +1,10 @@
+package net.vladimir.tutorialmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TutorialModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
